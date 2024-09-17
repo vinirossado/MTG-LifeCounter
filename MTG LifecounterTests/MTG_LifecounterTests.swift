9ef1b744cb1541cc0fbd5343adf30db5a3131b79
Vinicius Rossado <vinicius.rossado@riverty.com>
2024-09-17 20:20:45 +0300

    Initial Commit

---
+++ MTG LifecounterTests/MTG_LifecounterTests.swift
@@ -0,0 +1,16 @@
+//
+//  MTG_LifecounterTests.swift
+//  MTG LifecounterTests
+//
+//  Created by Vinicius Rossado on 17.09.2024.
+//
+
+import Testing
+
+struct MTG_LifecounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
